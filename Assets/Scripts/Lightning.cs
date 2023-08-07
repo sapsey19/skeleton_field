e7160b8c59ebc307a70077a40fab80a8947efc96
TaylorBradshaw <taylorcolebradshaw@gmail.com>
2023-08-06 20:39:00 -0600

    ruined ur linnghting :)

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -9,145 +9,147 @@ public class Lightning : MonoBehaviour {
 
     public LayerMask enemyMask;
 
-    public Transform point1; //weapon position
-    public Transform point2; //cursor position
+    public Transform weaponPosition;
+    public Transform cursorPosition;
     public float chainRadius;
 
-    Transform point3;    //enemy position
-    List<int> enemyIDs = new List<int>();
+    Transform enemyPosition = null;
 
     [SerializeField]
     List<Vector3> chainPoints = new List<Vector3>(); //list of chain points for lightning chain effect 
 
-    bool hittingEnemy;
-    bool firing;
-
-    LineRenderer lr;
+    LineRenderer lineRenderer;
     MeshCollider meshCollider;
 
     List<Collider> colliders = new List<Collider>();
     List<int> hitEnemyIds = new List<int>();
 
-    public List<Collider> GetColliders() { return colliders; }
-
     private void Start() {
         meshCollider = gameObject.GetComponent<MeshCollider>();
         meshCollider.enabled = false;
 
-        lr = GetComponent<LineRenderer>();
-        lr.enabled = false;
-
-        point3 = null;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
     }
 
     private void Update() {
         if (Input.GetMouseButton(0)) {
-            meshCollider.enabled = true;
-            firing = true;
-
+            Fire();
         }
         if (Input.GetMouseButtonUp(0)) {
-            meshCollider.enabled = false;
-            lr.enabled = false;
-            firing = false;
-            colliders.Clear();
-            enemyIDs.Clear();
-            chainPoints.Clear();
+            ResetLightning();
         }
-
-        // Lightning latches on to nearest enemy in hitbox
-        FindClosestEnemyInHitbox();
     }
 
-    private void LateUpdate() {
-        if (firing) {
-            if (hittingEnemy) {
-                DrawCurvedLine();
-            }
-            else {
+    private void Fire() {
+            meshCollider.enabled = true;
+
+            Collider closestEnemy = FindClosestEnemyInHitbox();
+
+            if (closestEnemy == null) {
+                lineRenderer.positionCount = 2;
+                lineRenderer.SetPosition(0, weaponPosition.position);
+                lineRenderer.SetPosition(1, cursorPosition.position);
+                DrawLine(); // draw line to cursor
+            } else {
+                chainPoints.Clear();
+                chainPoints.Add(weaponPosition.position);
+
+                hitEnemyIds.Add(closestEnemy.gameObject.GetInstanceID());
+                chainPoints.Add(closestEnemy.transform.position);
+
+                lineRenderer.positionCount = chainPoints.Count;
+                lineRenderer.SetPositions(chainPoints.ToArray());
                 DrawLine();
+
+                Chain(closestEnemy);
             }
-        }
     }
 
-    private void FindClosestEnemyInHitbox() {
-        List<Collider> inHitboxColliders = GetColliders();
+    // Chain lightning to surrounding enemies
+    private void Chain(Collider enemy) {
+        if (enemy == null) {
+            return;
+        }
 
+        Collider closestEnemy = FindClosestEnemy(enemy.transform.position);
+        if (!closestEnemy) {
+            return;
+        }
+
+        hitEnemyIds.Add(closestEnemy.gameObject.GetInstanceID());
+        chainPoints.Add(closestEnemy.transform.position);
+
+        lineRenderer.positionCount = chainPoints.Count;
+        lineRenderer.SetPositions(chainPoints.ToArray());
+        DrawLine();
+        Chain(closestEnemy);
+    }
+
+    private void ResetLightning() {
+        meshCollider.enabled = false;
+        lineRenderer.enabled = false;
+        colliders.Clear();
+        hitEnemyIds.Clear();
+        chainPoints.Clear();
+    }
+
+    // Finds closest enemy to player's weapon position (must be inside of weapon hitbox)
+    private Collider FindClosestEnemyInHitbox() {
         float min = 10000f;
-        int i = 0;
 
-        Transform closestEnemy = null;
+        Collider closestEnemy = null;
 
-        foreach (Collider c in inHitboxColliders) {
-            float distance = Vector3.Distance(point1.position, c.transform.position);
+        foreach (Collider c in colliders) {
+            float distance = Vector3.Distance(weaponPosition.position, c.transform.position);
             if (distance < min) {
                 min = distance;
-                closestEnemy = c.transform;
-                i++;
+                closestEnemy = c;
             }
         }
 
-        point3 = closestEnemy;
-    }
-
-    private void OnDrawGizmos() {
-        //Debug.Log("on draw gizmos");
-        if (point3) {
-            Gizmos.DrawSphere(point3.position, chainRadius);
+        if (closestEnemy != null) {
+            enemyPosition = closestEnemy.transform;
         }
+
+        return closestEnemy;
     }
 
-    private void FindClosestEnemy() {
-        Collider[] nearbyEnemies = Physics.OverlapSphere(point3.position, chainRadius, enemyMask);
+    // Finds closest enemy to given position (max distance away is chainRadius)
+    private Collider FindClosestEnemy(Vector3 position) {
+        if (position == null) {
+            return null;
+        }
 
         float min = 10000f;
-        int i = 0;
-        int indexOfClosest = 0;
+        Collider closestEnemy = null;
 
-        Transform closestEnemy = null;
-
-        //Debug.Log(nearbyEnemies.Length + " " + point3.position);
+        Collider[] nearbyEnemies = Physics.OverlapSphere(position, chainRadius, enemyMask);
 
         foreach (Collider c in nearbyEnemies) {
-            if (!enemyIDs.Contains(c.GetInstanceID())) {
-                Debug.Log("here");
-                float distance = Vector3.Distance(transform.position, c.transform.position);
-                if (distance < min) {
-                    min = distance;
-                    closestEnemy = c.transform;
-                    indexOfClosest = i;
-                }
-                enemyIDs.Add(c.GetInstanceID());
+
+            if (hitEnemyIds.Contains(c.gameObject.GetInstanceID())) {
+                continue; // skip to next collider
             }
-            i++;
+
+            float distance = Vector3.Distance(transform.position, c.transform.position);
+            if (distance < min) {
+                min = distance;
+                closestEnemy = c;
+            }
         }
 
-        if (closestEnemy!=null && !enemyIDs.Contains(closestEnemy.GetInstanceID())) {
-            chainPoints.Add(closestEnemy.position);
-        }
+        return closestEnemy;
     }
 
     private void DrawLine() {
-        lr.enabled = true;
-        lr.positionCount = 2;
-        lr.SetPosition(0, point1.position);
-        lr.SetPosition(1, point2.position);
+        lineRenderer.enabled = true;
     }
 
-    private void DrawCurvedLine() {
-        lr.enabled = true;
-        lr.positionCount = 3;
-        lr.SetPosition(0, point1.position);
-        lr.SetPosition(1, point2.position);
-        lr.SetPosition(2, point3.position);
-
-        BezierCurve();
-
-        FindClosestEnemy();
-
-        lr.positionCount += chainPoints.Count;
-
-        lr.SetPositions(chainPoints.ToArray());
+    private void OnDrawGizmos() {
+        if (enemyPosition) {
+            Gizmos.DrawWireSphere(enemyPosition.position, chainRadius);
+        }
     }
 
     private void OnTriggerStay(Collider other) {
@@ -155,29 +157,13 @@ public class Lightning : MonoBehaviour {
         if (hitObject.layer == LayerMask.NameToLayer("Enemy")) {
             if (!colliders.Contains(other))
                 colliders.Add(other);
-
-            lr.enabled = true;
-            hittingEnemy = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
-            hittingEnemy = false;
             colliders.Remove(other);
         }
-    }
-
-    private void BezierCurve() {
-        List<Vector3> pointList = new List<Vector3>();
-        for (float ratio = 0; ratio <= 1; ratio += 1.0f / 12) {
-            Vector3 tangentLineVertex1 = Vector3.Lerp(point1.position, point2.position, ratio);
-            Vector3 tangentLineVertex2 = Vector3.Lerp(point2.position, point3.position, ratio);
-            Vector3 bezierPoint = Vector3.Lerp(tangentLineVertex1, tangentLineVertex2, ratio);
-            pointList.Add(bezierPoint);
-        }
-        lr.positionCount = pointList.Count;
-        lr.SetPositions(pointList.ToArray());
     }
 
 }
